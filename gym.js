5dc28573d0917a54f04bb96719f2ba9ace570f60
Madiemad <77320586+Madiemad@users.noreply.github.com>
2021-05-25 19:31:34 +0530

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,47 @@
+obj=[];
+function back1(){
+    window.location="./index.html"
+}
+function preload(){
+    toi1=loadImage("https://i.postimg.cc/j5ff1nrf/gym.jpg");
+}
+function setup(){
+    canvas=createCanvas(320,240);
+    canvas.position(460,340);
+    coco=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("stat").innerHTML="STATUS: IT'S YOUR PROBLEM I AM STILL LOADING"
+}
+function modelLoaded(){
+    alert("THESE ARE DEVELOPERS EFFORTS THAT MODEL LOADED");
+    status=true;
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log("IT'S AGAIN YOUR PROBLEM THAT I HAVE AN ERROR! YOU WILL NOT REMEMBER IF I TELL YOU THE RESULT "+error);
+    }else{
+        obj=results;
+        document.getElementById("dect").innerHTML="OBJECTS DETECTED: "+obj.length;
+        console.log(results);
+    }
+}
+function draw(){
+    image(toi1,0,0,320,240);
+    r=random(255);
+    g=random(255);
+    b=random(255);
+    if(status != false){
+        coco.detect(toi1,gotResult);
+        for(var i=0; i<obj.length;i++){
+            fill(r,g,b);
+            $("#stat").html("STATUS: DEVELOPER'S EFFORTS ONLY!");
+            $("#nob").html("NUMBER OF OBJECCTS DETECTED: "+obj.length);
+            conf=floor(obj[i].confidence*100);
+            text(obj[i].label+" "+ conf+"%",obj[i].x+15,obj[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(obj[i].x,obj[i].y,obj[i].width,obj[i].height);
+            
+        }
+    }   
+}
---
+++ source.js
@@ -0,0 +1,15 @@
+function image1(){
+    window.location="tv.html";
+}
+function image2(){
+    window.location="dc.html";
+}
+function image3(){
+    window.location="goe.html";
+}
+function image4(){
+    window.location="joota.html";
+}
+function image5(){
+    window.location="gym.html";
+}
